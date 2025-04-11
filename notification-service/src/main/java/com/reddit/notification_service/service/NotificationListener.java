964c8b00d03d81318686cb5f9333599e6553f85f
Pratik Thatte <pratikthatte98@gmail.com>
2025-04-10 18:46:13 -0700

    Spring AMQP changes

--- notification-service/src/main/java/com/reddit/notification_service/service/NotificationListener.java
+++ notification-service/src/main/java/com/reddit/notification_service/service/NotificationListener.java
@@ -3,13 +3,26 @@ package com.reddit.notification_service.service;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.reddit.notification_service.config.RabbitMQConfig;
 import com.reddit.notification_service.dto.NotificationDto;
 
 @Service
 public class NotificationListener {
-	@RabbitListener(queues = RabbitMQConfig.NOTIFICATION_QUEUE)
-	public void handleNotification(NotificationDto message) {
-		System.out.println("New notification: " + message.getMessage() + " for user " + message.getUserId());
+	private final ObjectMapper objectMapper;
+	public NotificationListener(ObjectMapper objectMapper) {
+		super();
+		this.objectMapper = objectMapper;
 	}
+	@RabbitListener(queues = RabbitMQConfig.NOTIFICATION_QUEUE)
+	public void handleNotification(String message) {
+		try {
+		NotificationDto dto = objectMapper.readValue(message, NotificationDto.class);
+        System.out.println("Received notification: " + dto.getMessage());
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
